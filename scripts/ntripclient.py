2eda07bc87265eae0eec72851bea259862e59756
Grunde Nyvoll <grunde.nyvoll@gmail.com>
2022-03-08 15:06:21 +0100

    Changed so that position is continuously updated based on NavSatFixed msgs

--- scripts/ntripclient.py
+++ scripts/ntripclient.py
@@ -15,6 +15,15 @@ class ntripconnect(Thread):
         super(ntripconnect, self).__init__()
         self.ntc = ntc
         self.stop = False
+        if self.ntc.gps_topic is not None and self.ntc.gps_topic != '':
+            self.fixTopic = rospy.get_param('~gps_topic')
+            self._navsatfixed_sub = rospy.Subscriber(self.fixTopic, NavSatFix, self.navSatFixedCB, queue_size=1)
+
+    def navSatFixedCB(self, msg):
+        if msg.status.status >= 0:
+            self.ntc.latitude = msg.latitude
+            self.ntc.longitude = msg.longitude
+            
 
     def run(self):
         authenticationScheme = 'Basic '
@@ -35,22 +44,39 @@ class ntripconnect(Thread):
         rmsg = Message()
         while not self.stop:
             data = response.read(1)
-            if data!=chr(211).encode('latin-1'):
-                continue
-            l1 = ord(response.read(1))
-            l2 = ord(response.read(1))
-            pkt_len = ((l1&0x3)<<8)+l2
+            if len(data) != 0:
+                if data!=chr(211).encode('latin-1'):
+                    continue
+                l1 = ord(response.read(1))
+                l2 = ord(response.read(1))
+                pkt_len = ((l1&0x3)<<8)+l2
 
-            pkt = response.read(pkt_len)
-            parity = response.read(3)
-            if len(pkt) != pkt_len:
-                rospy.logerr("Length error: {} {}".format(len(pkt), pkt_len))
-                continue
+                pkt = response.read(pkt_len)
+                parity = response.read(3)
+                if len(pkt) != pkt_len:
+                    rospy.logerr("Length error: {} {}".format(len(pkt), pkt_len))
+                    continue
 
-            rmsg.header.seq += 1
-            rmsg.header.stamp = rospy.get_rostime()
-            rmsg.message = data + chr(l1).encode("latin-1") + chr(l2).encode("latin-1") + pkt + parity
-            self.ntc.pub.publish(rmsg)
+                rmsg.header.seq += 1
+                rmsg.header.stamp = rospy.get_rostime()
+                rmsg.message = data + chr(l1).encode("latin-1") + chr(l2).encode("latin-1") + pkt + parity
+                self.ntc.pub.publish(rmsg)
+            else:
+                "If zero length data, close connection and reopen it"
+                "Server periodically needs new gga, otherwise it stops sending, so we fix it here."
+                connection.close()
+                
+                # Update gga with current location
+                self.ntc.nmea_gga_string = generate_gga_string(self.ntc.latitude, self.ntc.longitude) 
+                rospy.loginfo(self.ntc.nmea_gga_string )
+                try:
+                    connection = HTTPConnection(self.ntc.ntrip_server + ':' + str(self.ntc.ntrip_port))
+                    connection.request('GET', '/'+self.ntc.ntrip_stream, self.ntc.nmea_gga_string, headers)
+                    response = connection.getresponse()
+                    if response.status != 200: raise Exception("Connection error. Got HTTP response status " + str(response.status))
+                except:
+                    pass # Need try-catch to recover from lost internet connection...
+            
 
         connection.close()
 
@@ -68,7 +94,7 @@ class ntripclient:
         self.ntrip_stream = rospy.get_param('~ntrip_stream', 'MSM_NEAR')
         self.gps_topic = None
         if rospy.has_param('~gps_topic'):
-            self.gps_topic = rospy.get_param('~gps_topic', '/ublox/fix')
+            self.gps_topic = rospy.get_param('~gps_topic')
         self.sub = None
 
         if self.gps_topic is not None and self.gps_topic != '':
@@ -81,7 +107,7 @@ class ntripclient:
             rospy.loginfo('Using coordinates from params')
             self.latitude = rospy.get_param('~latitude',63.416108)
             self.longitude = rospy.get_param('~longitude', 10.401436)
-            self.nmea_gga_string = self.generate_gga_string()
+            self.nmea_gga_string = generate_gga_string(self.latitude, self.longitude)
             self.connection = ntripconnect(self)
             self.connection.start()
 
@@ -91,40 +117,6 @@ class ntripclient:
         self.connection = None
 
 
-    def generate_gga_string(self):
-        """
-        Generates NMEA GGA message to send to the NTRIP caster
-        """
-
-        now = datetime.datetime.utcnow()
-        latitude = int(self.latitude) * 100 + \
-            (self.latitude - int(self.latitude)) * 60
-        longitude = int(self.longitude) * 100 + \
-            (self.longitude - int(self.longitude)) * 60
-        latitude_sign = 'N' if self.latitude >= 0 else 'S'
-        longitude_sign = 'E' if self.longitude >= 0 else 'W'
-
-        nmeadata = 'GPGGA,%02d%02d%04.1f,%09.4f,%s,%010.4f,%s,1,12,1.0,0.0,M,0.0,M,,' % (now.hour, now.minute, now.second, latitude, latitude_sign, longitude, longitude_sign)
-        
-        csum = 0
-        for c in nmeadata:
-               # XOR'ing value of csum against the next char in line
-               # and storing the new XOR value in csum
-               if ord(c)!=',':
-                  csum ^= ord(c)
-
-        #convert hex characters to upper case
-        csum = hex(csum).upper() 
-
-        #add 0x0 if checksum value is less than 0x10
-        if len(csum)==3:	
-            csum='0'+csum[2]
-        else:
-            csum=csum[2:4]
-
-        nmeastring = '$'+nmeadata+'*'+csum
-                
-        return(nmeastring)
 
     def gps_fix_cb(self, msg):
         """
@@ -133,7 +125,7 @@ class ntripclient:
         if msg.status.status >= 0:
             self.latitude = msg.latitude
             self.longitude = msg.longitude
-            self.nmea_gga_string = self.generate_gga_string()
+            self.nmea_gga_string = generate_gga_string(self.latitude, self.longitude)
             rospy.loginfo('Got position information with 3D fix, starting relaying RTCM data')
             self.connection = ntripconnect(self)
             self.connection.start()
@@ -146,6 +138,41 @@ class ntripclient:
         rospy.spin()
         if self.connection is not None:
             self.connection.stop = True
+
+def generate_gga_string(latitude, longitude):
+    """
+    Generates NMEA GGA message to send to the NTRIP caster
+    """
+
+    now = datetime.datetime.utcnow()
+    latitude = int(latitude) * 100 + \
+        (latitude - int(latitude)) * 60
+    longitude = int(longitude) * 100 + \
+        (longitude - int(longitude)) * 60
+    latitude_sign = 'N' if latitude >= 0 else 'S'
+    longitude_sign = 'E' if longitude >= 0 else 'W'
+
+    nmeadata = 'GPGGA,%02d%02d%04.1f,%09.4f,%s,%010.4f,%s,1,12,1.0,0.0,M,0.0,M,,' % (now.hour, now.minute, now.second, latitude, latitude_sign, longitude, longitude_sign)
+    
+    csum = 0
+    for c in nmeadata:
+            # XOR'ing value of csum against the next char in line
+            # and storing the new XOR value in csum
+            if ord(c)!=',':
+                csum ^= ord(c)
+
+    #convert hex characters to upper case
+    csum = hex(csum).upper() 
+
+    #add 0x0 if checksum value is less than 0x10
+    if len(csum)==3:	
+        csum='0'+csum[2]
+    else:
+        csum=csum[2:4]
+
+    nmeastring = '$'+nmeadata+'*'+csum
+            
+    return(nmeastring)
 
 if __name__ == '__main__':
     c = ntripclient()
